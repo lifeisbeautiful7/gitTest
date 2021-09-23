d42ff8ca841679944f640ae011a9b07350452779
Jeremy <jeremy@ck.com>
2021-09-23 09:38:34 +0800

    first commit1

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Jeremy Chen
+ * @create 2021-09-23 9:35
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
